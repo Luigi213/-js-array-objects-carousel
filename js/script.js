7d6503e95d329789b29e3cd8d511cd155d21cebf
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-14 14:25:28 +0100

    add the array object

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Consegna:
+// Dato un array di oggetti letterali con:
+// - url dell’immagine
+// - titolo
+// - descrizione
+//Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.   Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+let videogamesArray = [
+    {
+        img: '01.webp',
+        title: 'Spiderman',
+        descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum harum numquam?',
+    },
+    {
+        img: '02.webp',
+        title: 'Ratchet & Clank',
+        descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum harum numquam?',
+    },
+    {
+        img: '03.webp',
+        title: 'Fortnite',
+        descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum harum numquam?',
+    },
+    {
+        img: '04.webp',
+        title: 'Stray',
+        descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum harum numquam?',
+    },
+    {
+        img: '05.webp',
+        title: 'Avengers',
+        descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum harum numquam?',
+    }
+]
