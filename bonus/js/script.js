abcd5ced492f8568777209d61149ab8985d217a4
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-14 17:05:09 +0100

    Done bonus in the folder bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,6 +5,8 @@
 // - descrizione
 //Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello. Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
 //Milestone 2: Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+// BONUS 1: Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+// BONUS 2: Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 let videogamesArray = [
     {
         img: '01.webp',
@@ -45,6 +47,9 @@ const active = document.getElementsByClassName('item');
 let itemsActive = 0;
 active[itemsActive].classList.add('active');
 
+const activeThum = document.getElementsByClassName('image-thum');
+activeThum[itemsActive].classList.add('active-thum')
+
 const down = document.getElementById('down');
 const up = document.getElementById('up');
 
@@ -55,6 +60,7 @@ down.addEventListener('click', autoplay);
 up.addEventListener('click', function(){
     active[itemsActive].classList.remove('active');
     active[itemsActive].classList.remove('active');
+    activeThum[itemsActive].classList.remove('active-thum')
     if(itemsActive == 0){
         itemsActive = 4;
     }
@@ -63,11 +69,13 @@ up.addEventListener('click', function(){
     }
     active[itemsActive].classList.add('active');
     active[itemsActive].classList.add('active');
+    activeThum[itemsActive].classList.add('active-thum')
 });
 
 function autoplay(){
     active[itemsActive].classList.remove('active');
     active[itemsActive].classList.remove('active');
+    activeThum[itemsActive].classList.remove('active-thum')
     if(itemsActive == 4){
         itemsActive = 0;
     }
@@ -76,4 +84,5 @@ function autoplay(){
     }
     active[itemsActive].classList.add('active');
     active[itemsActive].classList.add('active');
+    activeThum[itemsActive].classList.add('active-thum')
 };
